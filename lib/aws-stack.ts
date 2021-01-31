191715791bdb40494d1b6016b889f8ddc8380df9
Syed Muhammed Bilal <intergo878@gmail.com>
2021-01-31 13:29:06 +0500

    new cdk stacks s3-buckets

--- lib/aws-stack.ts
+++ lib/aws-stack.ts
@@ -1,9 +1,12 @@
 import * as cdk from '@aws-cdk/core';
+import { Bucket } from "@aws-cdk/aws-s3";
 
 export class AwsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+    });
   }
 }
